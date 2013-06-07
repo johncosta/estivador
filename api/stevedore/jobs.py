54a74ccfc85be90ca6a9e932a3fd10cd444eb97b
John Costa <john.costa@gmail.com>
2013-06-07 15:43:29 -0400

    add result detail creation

--- api/stevedore/jobs.py
+++ api/stevedore/jobs.py
@@ -4,7 +4,7 @@ import docker
 
 from . import constants
 from . import utils
-from .models import Task
+from .models import Task, ResultDetail
 from requests import HTTPError
 
 random.seed()
@@ -21,6 +21,8 @@ def execute_worker(task_id, result_id, command, session=None, *args, **kwargs):
         session = utils.create_db_session(
             database=database, database_options=database_options)
 
+        detail = ResultDetail.create_unique_resultdetail(session, result_id)
+
         # TODO: This will be expensive if there are a lot of workers.
         #       We should pass all the information we need
         task = Task.find_by_id(session, task_id)
@@ -31,15 +33,20 @@ def execute_worker(task_id, result_id, command, session=None, *args, **kwargs):
 
         client = docker.Client()
         try:
+            detail.update_status(session, constants.RUNNING)
             container = client.create_container(task.repository, command)
             client.start(container)
             result = client.wait(container['Id'])
+            detail.update_status(session, constants.COMPLETE)
             print result
         except HTTPError, httpe:
             print("Error: {0}".format(httpe))
-            task.update_status(session, constants.ERROR)
-        else:
-            task.update_status(session, constants.COMPLETE)
+            # TODO we really want to update the status of the "result"
+            # elseware, maybe a cleanup job?
+            #task.update_status(session, constants.ERROR)
+            detail.update_status(session, constants.ERROR)
+        # else:
+        #     task.update_status(session, constants.COMPLETE)
 
     except Exception, e:
         print("Error: {0}".format(e))
